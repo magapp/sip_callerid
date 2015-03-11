7fdecc99f9b429a1edd4b9a10b86f6c62c594743
Magnus Appelquist <magnus.appelquist@cloudnet.se>
2015-03-11 11:00:48 +0100

    Fixed bug and added daemon support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 SIP caller id
 =============
-This is a simple program that register itself to a SIP server. On incoming calls a script will be executed with the incoming caller id as argument.
+This is a simple program that register itself to a SIP server. On incoming calls an URL will be called with the incoming caller id as argument.
 
 Tested to register with Asterisk and wx3 pbx.
 
@@ -10,12 +10,10 @@ Most of the time it is good to use a virtualenv. Type something like this:
 
 	git clone https://github.com/magapp/sip_callerid.git
 	virtualenv env
-	env/bin/pip install twisted
-	env/bin/python sip_callerid.py --server asterisk.domain.com --number 0812345678 --authname 0812345678 --password secret --execute 'echo CALLERID is calling'
+	env/bin/pip install twisted requests
+	env/bin/python sip_callerid.py --server asterisk.domain.com --number 0812345678 --authname 0812345678 --password secret --url 'www.host.com/&callerid=CALLERID'
 
-sip_callerid will now register itself to asterisk.domain.com and for example print the text below on incoming phone calls:
-
-	0887654321 is calling
+sip_callerid will now register itself to asterisk.domain.com and call www.host.com on incoming calls.
 
 
 Usage
@@ -24,11 +22,9 @@ There are some arguments you can use. Type --help for more information.
 
 You can for example trigg a Jenkins job that do some caller id lookup and other nice stuff if you like. Example:
 
-	... --execute 'curl "https://jenkins.domain.com/buildByToken/buildWithParameters/?job=callerid&token=abc&cause=sip_callerid&FROM_NUMBER=CALLERID"'
+	... --url 'https://jenkins.domain.com/buildByToken/buildWithParameters/?job=callerid&token=abc&cause=sip_callerid&FROM_NUMBER=CALLERID'
 
+Bugs
+====
 Note that sometimes the port is not released correct on exit. I don't know exaclty why. If that happens, just run a tool like "iptstate" and remove all sockets in TIME_WAIT on port 5060.
-
-Todo
-===
-Add arguements so the program can become a daemon. You can always execute in background by appending &.
 
--- sip_callerid.py
+++ sip_callerid.py
@@ -1,22 +1,30 @@
-import argparse, sys, os, re, string, random, hashlib, socket
+#!/usr/bin/env python
+import argparse, sys, os, re, string, random, hashlib, socket, atexit, time
+import requests
 import logging, logging.handlers
 from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
 from twisted.names import client
 from twisted.internet.task import LoopingCall
 
+from signal import SIGTERM
+
+import logging
+import logging.handlers
+
 app_name="sip_callerid"
 app_version="1.0"
 
 def main():
     parser = argparse.ArgumentParser(prog=app_name, description="Magnus 2014-08-18")
-    parser.add_argument('--server', dest='server', action='store', required=True, help='SIP server to connect to')
-    parser.add_argument('--port', dest='port', action='store', type=int, required=False, default=5060, help='SIP server port to use')
-    parser.add_argument('--number', dest='number', action='store', required=True, help='Number to authenticate with')
-    parser.add_argument('--authname', dest='authname', action='store', required=True, help='Name to authenticate with')
-    parser.add_argument('--password', dest='password', action='store', required=True, help='Password to use')
-    parser.add_argument('--realm', dest='realm', action='store', required=False, help='Realm')
-    parser.add_argument('--execute', dest='execute', action='store', required=False, help='Script to execute on incoming call. CALLERID will be replace with callerid.')
+    parser.add_argument('--server', action='store', required=True, help='SIP server to connect to')
+    parser.add_argument('--port', action='store', type=int, required=False, default=5060, help='SIP server port to use')
+    parser.add_argument('--number', action='store', required=True, help='Number to authenticate with')
+    parser.add_argument('--authname', action='store', required=True, help='Name to authenticate with')
+    parser.add_argument('--password', action='store', required=True, help='Password to use')
+    parser.add_argument('--realm', action='store', required=False, help='Realm')
+    parser.add_argument('--url', action='store', required=False, help='URL to call on incoming call. CALLERID will be replace with callerid.')
+    parser.add_argument('--pid-file', action='store', required=False, default="/tmp/callerid.pid", help='PID file')
     parser.add_argument('-V', '--version', action='version', version="%(prog)s version "+app_version)
 
     args = parser.parse_args()
@@ -25,11 +33,11 @@ def main():
     args.server = ips.pop()
 
     logger = logging.getLogger(app_name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     handler = logging.handlers.SysLogHandler(address = '/dev/log')
     logger.addHandler(handler)
 
-    sip = SipClient(logger, args.server, args.port, args.number, args.authname, args.password, args.realm, args.execute)
+    sip = SipClient(logger, args.server, args.port, args.number, args.authname, args.password, args.realm, args.url)
     reactor.listenUDP(0, sip)
 
     def register_task():
@@ -41,7 +49,16 @@ def main():
 
     print "Connecting to SIP server. See syslog for more information."
 
-    reactor.run()
+    class SipCallerDaemon(Daemon):
+        def run(self, logger):
+            logger.info('Starting')
+            while True:
+                reactor.run()
+
+    daemon = SipCallerDaemon(args.pid_file)
+    if not daemon.start(logger):
+        print "Already running, restarting..."
+        daemon.restart(logger)
 
     sys.exit(0)
 
@@ -87,10 +104,10 @@ Allow: INVITE, ACK, CANCEL, BYE, OPTIONS, INFO, REFER, SUBSCRIBE, NOTIFY
 Content-Length: 0
 """
 
-    def __init__(self, logger, server, port, number, authname, password, realm=None, execute=None):
+    def __init__(self, logger, server, port, number, authname, password, realm=None, url=None):
         self.logger = logger
         self.realm = realm
-        self.execute = execute
+        self.url = url
         self.server = server
         self.port = port
         self.number = number
@@ -110,6 +127,7 @@ Content-Length: 0
         lines = data.split("\n")
         self.logger.debug(lines[0])
         if lines[0][0:7] == "SIP/2.0":
+
             status = lines[0][8:11]
             if status == "401":
                 if self.registration_counter > 5:
@@ -124,6 +142,7 @@ Content-Length: 0
                 ha1 = hashlib.md5(b""+self.authname+":"+self.realm+":"+self.password).hexdigest()
                 ha2 = hashlib.md5(b"REGISTER:sip:"+self.server).hexdigest()
                 self.response = hashlib.md5(b""+ha1+":"+self.nonce+":"+ha2).hexdigest()
+                self.seq = self.seq + 1
                 self.sendsip(self.msg_register_auth)
         if lines[0][0:7] == "OPTIONS":
             self.registration_counter = 0
@@ -135,7 +154,7 @@ Content-Length: 0
             if m: callid = m.groups()[0]
             m = re.search(".*CSeq: ([\d]+).*", data)
             if m: self.seq = int(m.groups()[0])
-            m = re.search(".*CSeq: ([\d]+).*", data)
+            #m = re.search(".*CSeq: ([\d]+).*", data)
             self.sendsip(self.msg_options, branch=branch, callid=callid, remote_tag=remote_tag)
 
         if lines[0][0:6] == "INVITE":
@@ -150,9 +169,9 @@ Content-Length: 0
                 else:
                     self.callerid_branch = m.groups()[0]
             self.logger.info("Incoming call from '%s'" % callerid)
-            if self.execute:
-                self.logger.debug("Executing %s" % self.execute.replace("CALLERID", callerid))
-                os.system(self.execute.replace("CALLERID", callerid))
+            if self.url:
+                self.logger.debug("Calling URL %s" % self.url.replace("CALLERID", callerid))
+                requests.get(self.url.replace("CALLERID", callerid))
 
         
     def sendsip(self, msg, branch=None, callid=None, remote_tag=None):
@@ -182,6 +201,136 @@ Content-Length: 0
                                                               "nonce": self.nonce}))
         return
 
+
+
+    
+class Daemon:
+        """
+        A generic daemon class.
+       
+        Usage: subclass the Daemon class and override the run() method
+        """
+        def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+                self.stdin = stdin
+                self.stdout = stdout
+                self.stderr = stderr
+                self.pidfile = pidfile
+       
+        def daemonize(self):
+                """
+                do the UNIX double-fork magic, see Stevens' "Advanced
+                Programming in the UNIX Environment" for details (ISBN 0201563177)
+                http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+                """
+                try:
+                        pid = os.fork()
+                        if pid > 0:
+                                # exit first parent
+                                sys.exit(0)
+                except OSError, e:
+                        sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+                        sys.exit(1)
+       
+                # decouple from parent environment
+                os.chdir("/")
+                os.setsid()
+                os.umask(0)
+       
+                # do second fork
+                try:
+                        pid = os.fork()
+                        if pid > 0:
+                                # exit from second parent
+                                sys.exit(0)
+                except OSError, e:
+                        sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+                        sys.exit(1)
+       
+                # redirect standard file descriptors
+                sys.stdout.flush()
+                sys.stderr.flush()
+                si = file(self.stdin, 'r')
+                so = file(self.stdout, 'a+')
+                se = file(self.stderr, 'a+', 0)
+                os.dup2(si.fileno(), sys.stdin.fileno())
+                os.dup2(so.fileno(), sys.stdout.fileno())
+                os.dup2(se.fileno(), sys.stderr.fileno())
+       
+                # write pidfile
+                atexit.register(self.delpid)
+                pid = str(os.getpid())
+                file(self.pidfile,'w+').write("%s\n" % pid)
+       
+        def delpid(self):
+                os.remove(self.pidfile)
+ 
+        def start(self, logger):
+                """
+                Start the daemon
+                """
+                # Check for a pidfile to see if the daemon already runs
+                try:
+                        pf = file(self.pidfile,'r')
+                        pid = int(pf.read().strip())
+                        pf.close()
+                except IOError:
+                        pid = None
+       
+                if pid:
+                        message = "pidfile %s already exist. Daemon already running?\n"
+                        sys.stderr.write(message % self.pidfile)
+                        return False
+               
+                # Start the daemon
+                self.daemonize()
+                self.run(logger)
+                return True 
+
+        def stop(self, logger):
+                """
+                Stop the daemon
+                """
+                # Get the pid from the pidfile
+                try:
+                        pf = file(self.pidfile,'r')
+                        pid = int(pf.read().strip())
+                        pf.close()
+                except IOError:
+                        pid = None
+       
+                if not pid:
+                        message = "pidfile %s does not exist. Daemon not running?\n"
+                        sys.stderr.write(message % self.pidfile)
+                        return # not an error in a restart
+ 
+                logger.info("Stopping")
+
+                # Try killing the daemon process       
+                try:
+                        while 1:
+                                os.kill(pid, SIGTERM)
+                                time.sleep(0.1)
+                except OSError, err:
+                        err = str(err)
+                        if err.find("No such process") > 0:
+                                if os.path.exists(self.pidfile):
+                                        os.remove(self.pidfile)
+                        else:
+                                print str(err)
+                                sys.exit(1)
+ 
+        def restart(self, logger):
+                """
+                Restart the daemon
+                """
+                self.stop(logger)
+                self.start(logger)
+ 
+        def run(self):
+                """
+                You should override this method when you subclass Daemon. It will be called after the process has been
+                daemonized by start() or restart().
+                """
 
 if __name__ == "__main__":
     main()
